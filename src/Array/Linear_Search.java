589475e5634ef7f66826864b55c228d2250f2366
Shreyash Kawale <kawalesn01@gmail.com>
2025-12-08 22:28:14 +0530

    linear Search

---
+++ src/Array/Linear_Search.java
@@ -0,0 +1,29 @@
+package Array;
+
+public class Linear_Search {
+
+	public static void main(String[] args) {
+		
+		int arr[] = { 8, 14, 36, 62, 94, 35};
+		
+		int search = 62;
+		
+		int index = -1;
+		
+		
+		for(int i=0; i < arr.length; i++){
+			
+			if(arr[i] == search) {
+				index = i;
+				break;
+			}
+			
+		}
+		
+		System.out.println("Element Present on Index: "+index);
+			
+	}
+
+}
+
+
