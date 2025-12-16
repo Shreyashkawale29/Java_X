23e35177b6e1a317484477ccde805c8d34e878c1
Shreyash Kawale <kawalesn01@gmail.com>
2025-12-16 11:45:37 +0530

    Factorial

---
+++ src/Number/Factorial.java
@@ -0,0 +1,20 @@
+package Number;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int number = 5;
+		int fact = 1;
+		
+		for(int i=1; i<= number; i++) {
+			
+			fact = fact * i;
+			
+		}
+		
+		System.out.println("Factorial Of "+number+": "+fact);
+
+	}
+
+}
